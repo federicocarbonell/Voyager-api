c19361ad367d1bd606d3622f037974ca99cac281
Fede <Federico@bankingly.com>
2021-10-25 22:19:35 -0300

    Add GetProductInfo endpoint and its corresponding layers and tests.

---
+++ Voyage/VoyageAPI/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using VoyageAPI.DTOs;
+using VoyageAPI.Logic;
+
+namespace VoyageAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductLogic _productLogic;
+
+        public ProductController(IProductLogic productLogic)
+        {
+            _productLogic = productLogic;
+        }
+
+        [HttpGet("{productId}", Name = "GetProductInfo")]
+        public ActionResult<ProductDTO> GetProductInfo([FromRoute] int productId)
+        {
+
+            ProductDTO product = _productLogic.GetProductInfo(productId);
+            if (product == null)
+            {
+                return NotFound("No Product found.");
+            }
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Voyage/VoyageAPI/Logic/ProductLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using VoyageAPI.Context;
+using VoyageAPI.DTOs;
+using VoyageAPI.Models;
+
+namespace VoyageAPI.Logic
+{
+    public class ProductLogic : IProductLogic
+    {
+        ApplicationDbContext _context;
+        public ProductLogic(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public ProductDTO GetProductInfo(int productId)
+        {
+            Product product = _context.Products.FirstOrDefault(p => p.Id == productId);
+            if (product == null)
+            {
+                return null;
+            }
+            ProductDTO productDTO = new ProductDTO
+            {
+                Id = product.Id,
+                Description = product.Description,
+                Name = product.Name,
+                Year = product.Year
+            };
+            return productDTO;
+        }
+    }
+}
