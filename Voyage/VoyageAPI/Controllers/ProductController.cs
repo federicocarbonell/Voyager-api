c8f35ef6684a4f8f4bfa87a5692f982588153818
Jose Pablo Goñi <josepablogoni@gmail.com>
2021-10-27 11:46:08 -0300

    Add controller to create new report

--- Voyage/VoyageAPI/Controllers/ProductController.cs
+++ Voyage/VoyageAPI/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using VoyageAPI.DTOs;
 using VoyageAPI.Logic;
+using VoyageAPI.Models;
 
 namespace VoyageAPI.Controllers
 {
@@ -51,6 +52,28 @@ namespace VoyageAPI.Controllers
             }
 
             
+        }
+
+        [HttpPost("{productId}")]
+        public ActionResult PostReport([FromRoute] int productId, [FromBody] Report report)
+        {
+            try
+            {
+                _productLogic.AddReport(productId, report);
+                return Ok();
+            }
+            catch (IndexOutOfRangeException ind)
+            {
+                return BadRequest(ind.Message);
+            }
+            catch (ArgumentException arg)
+            {
+                return BadRequest(arg.Message);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
         }
     }
 }
--- Voyage/VoyageAPI/Logic/ProductLogic.cs
+++ Voyage/VoyageAPI/Logic/ProductLogic.cs
@@ -16,6 +16,34 @@ namespace VoyageAPI.Logic
             _context = context;
         }
 
+        public void AddReport(int productId, Report report)
+        {
+            Product product = _context.Products.FirstOrDefault(p => p.Id == productId);
+            if (product == null) throw new IndexOutOfRangeException("Incorrect product ID.");
+            Employee employee = _context.Employees.FirstOrDefault(e => e.Id == report.Employee.Id);
+            if (employee == null) throw new IndexOutOfRangeException("Incorrect employee ID.");
+            if (report.VisitDate == null) report.VisitDate = DateTime.Now;
+            else
+            {
+                if (report.VisitDate < DateTime.Now) throw new ArgumentException("The visit date cannot be greater than today.");
+            }
+            if (report.TimeArrival == null) report.TimeArrival = DateTime.Now;
+            if (report.TimeResolution == null) report.TimeResolution = DateTime.Now;
+            if(report.Summary == null) throw new ArgumentException("The report must contain a summary.");
+            if (report.Detail == null) throw new ArgumentException("The report must contain a details.");
+            if (report.Comment == null) throw new ArgumentException("The report must contain a comment.");
+            if(report.Images != null)
+            {
+                if(report.Images.Count == 0) throw new ArgumentException("The report must contain at least one image.");
+                foreach (Image image in report.Images)
+                {
+                    if (image.Path == null) report.Images.Remove(image);
+                }
+            }
+            else throw new ArgumentException("The report must contain at least one image.");
+            _context.Reports.Add(report);
+        }
+
         public ProductDTO GetProductInfo(int productId)
         {
             Product product = _context.Products.FirstOrDefault(p => p.Id == productId);
